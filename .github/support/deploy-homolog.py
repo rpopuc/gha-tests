3c8384406f87fc771f17f7992b1d3e260d553913
Robson P Oliveira <rpopuc@gmail.com>
2023-07-18 15:15:00 -0300

    feat: merge more than one branch at time

--- .github/support/deploy-homolog.py
+++ .github/support/deploy-homolog.py
@@ -1,18 +1,27 @@
 import sys
 import re
+import json
 
 errors = []
 
 def parseBranches(payload):
     matches = re.search("##\s+branches\s*([^#]*)", payload, re.IGNORECASE + re.MULTILINE)
-    return matches.group(1).strip() if matches != None else ''
+
+    # Strip matches by end line character and get an array of branches
+    branches = [branch.strip() for branch in matches.group(1).splitlines()]
+
+    # Remove empty branches
+    branches = list(filter(None, branches))
+
+    # strip whitespaces from each branch
+    return [branch.strip() for branch in branches]
 
 try:
     command = sys.argv[1]
     payload = sys.argv[2]
 
     if command == 'parse-branches':
-        print(parseBranches(payload))
+        print(json.dumps(parseBranches(payload)))
         exit()
 
     print('["invalid-command"]')
