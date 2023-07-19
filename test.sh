171ea4efb250bb84a817c6bdc995e5bf0e54ea1b
Robson P Oliveira <rpopuc@gmail.com>
2023-07-19 14:00:52 -0300

    feat: remove branch from list

--- test.sh
+++ test.sh
@@ -1,7 +1,4 @@
 branch=$1
-git log --pretty=format:"%h" main | grep `git log --pretty=format:"%h" -1 $branch` &> /dev/null
-if [ $? -eq 0 ]; then
-    echo "Branch $branch está na main"
-else
-    echo "Branch $branch não está na main"
-fi
+lista_branches=$(cat homolog_branches)
+lista_branches="${lista_branches//$branch/}"
+echo $lista_branches > homolog_branches_atualizado
